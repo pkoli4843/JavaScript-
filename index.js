81d6bae450ed1569a19dae3d4b49a0ec54a5b8a5
Pankaj Koli <66156023+pkoli4843@users.noreply.github.com>
2022-11-23 18:26:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function arithmatic(num1,num2){
+  console.log("Entered numbers :"+num1 +"&"+ num2);
+  var add = num1 + num2;
+  var sub = num1 + num2;
+  return add, sub;
+
+
+}
+
+var results = arithmatic(5,5);
+console.log("Your Results are :\n");
+console.log("Addition = "+ add);
+console.log("Substraction = "+ sub);
